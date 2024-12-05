5ceabd205a36554ceade5c43fff7a7c07c95cb1e
Henk Jan Pluim <henkjan@kxdesign.nl>
2024-12-05 15:28:20 +0100

    added day 5

---
+++ src/adventofcode-2024/Contest/day5/Day5.cs
@@ -0,0 +1,16 @@
+﻿using adventofcode_2024.Contracts;
+
+namespace adventofcode_2024.Contest.day5;
+
+public class Day5 : ISolver
+{
+    public string SolvePart1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string SolvePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
